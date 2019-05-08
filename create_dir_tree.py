fcb9124e8740628020bb6b8d01546d16b38d40ae
Aaron <aaron.j.m.chang@gmail.com>
2019-05-07 23:12:48 -0500

    works without O_DIRECT now

--- create_dir_tree.py
+++ create_dir_tree.py
@@ -17,10 +17,11 @@ if len(sys.argv) > 2:
 each_file_size = total_size // number_of_files
 each_file_size *= 1024 * 1024
 each_file_size -= each_file_size % 4096
+each_file_size //= 4096
 # write files in both dirs
 for i in range(number_of_files // 2):
-	command = "dd if=/dev/zero of=./sourcedir/file_%d bs=1 count=%d > /dev/null 2>&1" % (i, each_file_size)
+	command = "dd if=/dev/zero of=./sourcedir/file_%d bs=4096 count=%d > /dev/null 2>&1" % (i, each_file_size)
 	subprocess.check_output(command, shell=True)
 for i in range(number_of_files // 2, number_of_files):
-	command = "dd if=/dev/zero of=./sourcedir/level2dir/file_%d bs=1 count=%d < /dev/null 2>&1" % (i, each_file_size)
+	command = "dd if=/dev/zero of=./sourcedir/level2dir/file_%d bs=4096 count=%d < /dev/null 2>&1" % (i, each_file_size)
 	subprocess.check_output(command, shell=True)
